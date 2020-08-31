554576813bbd0bbf516e8535f864acf1011ce032
Shiva Rakesh <69103445+shivarakesh99@users.noreply.github.com>
2020-08-31 01:53:52 -0700

    Delete 2ndprogram.java

--- 2ndprogram.java
+++
@@ -1,8 +0,0 @@
-import com.*;
-packae mypackage;
-public class 2ndprogram{
-public static void main(String[] args){
-loanaccount acc=new loanaccount();
-transact acc=new credit();
-transact acc=new debit();
-}
